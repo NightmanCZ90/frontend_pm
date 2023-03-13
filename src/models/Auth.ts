86ef52a35d0d9b0d43699a4c275f3e7d969e248a
Jan Novak <novak.jan90@gmail.com>
2023-03-13 22:25:37 +0100

    feat: Implement SignIn page

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -17,13 +17,28 @@ export const auth = {
     setTokens: (tokens: Tokens | null) => ((state?: AuthState) => ({ ...state, tokens }))(),
   },
   effects: {
-    async signIn({ email, password }: { email: string, password: string }, dispatch?: any) {
+    async signIn(
+      { email, password }: { email: string, password: string },
+      dispatch?: any,
+    ) {
       const tokens = await RestApiClient.signIn({ email, password });
+
       if (tokens) {
         localStorage.setItem('jwt_token', JSON.stringify(tokens));
         (dispatch as typeof storeReducers).auth.setTokens(tokens);
       }
-      return tokens;
+    },
+
+    async signUp(
+      { email, password, confirmPassword }: { email: string, password: string, confirmPassword: string },
+      dispatch?: any,
+    ) {
+      const tokens = await RestApiClient.signUp({ email, password, confirmPassword });
+
+      if (tokens) {
+        localStorage.setItem('jwt_token', JSON.stringify(tokens));
+        (dispatch as typeof storeReducers).auth.setTokens(tokens);
+      }
     },
 
     signOut(_payload: any, dispatch?: any) {
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,11 +1,23 @@
-import { Component, createEffect, createResource, createSignal, Setter } from "solid-js";
+import { createForm, email, Field, Form, required, SubmitEvent } from "@modular-forms/solid";
+import { Button, CircularProgress, TextField, Typography } from "@suid/material";
+import { Component, createResource, createSignal, For, Setter } from "solid-js";
 import { useDispatch } from "../../store";
+import { tokens, useThemeContext } from "../../styles/theme";
+import { StyledSignIn, StyledSignInForm } from "./SignIn.styles";
 
-async function signIn() {
+async function signIn(payload: LoginForm | null) {
   const dispatch = useDispatch();
-  const tokens = await dispatch.auth.signIn({ email: 'test@test.com', password: 'heslo' });
 
-  return tokens;
+  if (!payload) {
+    return;
+  }
+
+  return dispatch.auth.signIn({ email: payload.email, password: payload.password });
+}
+
+export type LoginForm = {
+  email: string;
+  password: string;
 }
 
 interface ISignInProps {
@@ -13,19 +25,117 @@ interface ISignInProps {
 }
 
 const SignIn: Component<ISignInProps> = (props) => {
-  const [trigger, setTrigger] = createSignal(false)
-  const [tokens] = createResource(trigger, signIn);
+  const [mode] = useThemeContext();
+  const colors = () => tokens(mode());
 
-  createEffect(() => {
-    console.log(tokens.loading);
-  });
+  const loginForm = createForm<LoginForm>();
+  const [formData, setFormData] = createSignal<LoginForm | null>(null);
+
+  const [authentication] = createResource(formData, (e) => signIn(e));
+
+  const handleSubmit = (values: LoginForm, event: SubmitEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setFormData(values);
+  }
 
   return (
-    <div>
-      <h1>SignIn - Login</h1>
-      <button onClick={() => setTrigger(true)}>Sign in</button>
-      <button onClick={() => props.setShowLogin(false)}>Register new user</button>
-    </div>
+    <StyledSignIn class="SignIn">
+
+      <StyledSignInForm colors={colors()}>
+        <Typography variant="h1" fontWeight={500}>port/fall.io</Typography>
+        <br />
+        <Form of={loginForm} onSubmit={handleSubmit}>
+
+          <Field
+            of={loginForm}
+            name="email"
+            validate={[
+              required('Please enter your email.'),
+              email('Please enter a valid email address.')
+            ]}
+          >
+            {(field) =>
+              <TextField
+                {...field.props}
+                type="email"
+                color="secondary"
+                // Need to overwrite onInput and onChange props for Modular Forms and SUID to comunicate together
+                onInput={() => undefined}
+                onChange={e => {
+                  field.props.onInput(e as any);
+                  field.props.onChange(e)
+                }}
+                variant="outlined"
+                component="input"
+                label="E-mail"
+                required
+                error={Boolean(field.error)}
+                helperText={field.error}
+              />}
+          </Field>
+
+          <Field
+            of={loginForm}
+            name="password"
+            validate={[
+              required('Please enter your password.'),
+              // TODO: Put this to sign Up
+              // minLength(8, 'Your password must have 8 characters or more.')
+            ]}
+          >
+            {(field) =>
+              <TextField
+                {...field.props}
+                type="password"
+                color="secondary"
+                // Need to overwrite onInput and onChange props for Modular Forms and SUID to comunicate together
+                onInput={() => undefined}
+                onChange={e => {
+                  field.props.onInput(e as any);
+                  field.props.onChange(e)
+                }}
+                variant="outlined"
+                component="input"
+                label="Password"
+                required
+                error={Boolean(field.error)}
+                helperText={field.error}
+              />}
+          </Field>
+
+          <div class="signin-form--button">
+            <Button
+              type="submit"
+              fullWidth
+              color="secondary"
+              variant="contained"
+              disabled={authentication.loading || loginForm.invalid}
+            >
+              {authentication.loading ? (<CircularProgress size={24} />) : "Sign in"}
+            </Button>
+          </div>
+
+          {authentication.error && authentication.error.message
+            ? Array.isArray(authentication.error.message)
+              ? <For each={authentication.error.message}>
+                {(item) => <span class="error-message">{item}</span>}
+              </For>
+              : <span class="error-message">{authentication.error?.message}</span>
+            : null}
+
+          <div class="signin-form--buttons">
+            <Button color="secondary" onClick={() => props.setShowLogin(false)}>Create new account</Button>
+
+            {/* For testing purposes - faster signin */}
+            <button type="button" onClick={() => setFormData({ email: 'test@test.com', password: 'heslo' })}>Sign in as Test</button>
+          </div>
+
+        </Form>
+      </StyledSignInForm>
+
+    </StyledSignIn>
   )
 }
 
