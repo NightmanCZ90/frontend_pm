cf1f03afbe3fc0effe568145d67bba0f0bc6dbbb
Jan Novak <novak.jan90@gmail.com>
2023-03-15 00:04:00 +0100

    feat: Create user menu with menu items

--- src/pages/globals/Topbar/Topbar.tsx
+++ src/pages/globals/Topbar/Topbar.tsx
@@ -1,6 +1,6 @@
-import { Component, Show } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 import { LightModeOutlined, DarkModeOutlined, NotificationsOutlined, SettingsOutlined, PersonOutlined } from '@suid/icons-material'
-import { IconButton } from "@suid/material";
+import { IconButton, Menu, MenuItem } from "@suid/material";
 
 import { useThemeContext } from "../../../styles/theme";
 import { StyledTopbar } from "./Topbar.styles";
@@ -9,6 +9,11 @@ import { useDispatch } from "../../../store";
 const Topbar: Component = () => {
   const [mode, toggleColorMode] = useThemeContext();
   const dispatch = useDispatch();
+
+  const [anchorEl, setAnchorEl] = createSignal<HTMLElement | null>(null);
+
+  const openUserMenu = () => Boolean(anchorEl());
+  const closeUserMenu = () => setAnchorEl(null);
 
   return (
     <StyledTopbar>
@@ -28,11 +33,23 @@ const Topbar: Component = () => {
           <SettingsOutlined />
         </IconButton>
 
-        <IconButton>
+        <IconButton
+          onClick={(event) => {
+            setAnchorEl(event.currentTarget);
+          }}
+        >
           <PersonOutlined />
         </IconButton>
 
-        <button onClick={dispatch.auth.signOut}>Sign out</button>
+        <Menu
+          id="user-menu"
+          anchorEl={anchorEl()}
+          open={openUserMenu()}
+          onClose={closeUserMenu}
+        >
+          <MenuItem onClick={closeUserMenu}>My account</MenuItem>
+          <MenuItem onClick={dispatch.auth.signOut}>Sign out</MenuItem>
+        </Menu>
 
       </div>
     </StyledTopbar>
