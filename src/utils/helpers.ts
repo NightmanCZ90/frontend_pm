03a09ce0fc3db9d168ffe8d3825be453d507e475
Jan Novak <novak.jan90@gmail.com>
2023-03-21 23:27:08 +0100

    data: Add portfolio helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { FieldElement } from "@modular-forms/solid";
 import { JSX } from "solid-js";
+import { User } from "../types";
 
 export const remapFieldProps = (fieldProps: {
   name: string;
@@ -13,3 +14,33 @@ export const remapFieldProps = (fieldProps: {
   onBlur: fieldProps.onBlur,
   onChange: (e: any) => { fieldProps.onChange(e); fieldProps.onInput(e) }
 });
+
+export const generateGreenRedClass = (value: number) => {
+  if (value > 0) return 'green';
+  if (value < 0) return 'red';
+  return '';
+}
+
+export const generateUserName = (user?: User) => {
+  const { firstName, lastName, email } = user || {};
+  if (firstName && lastName) return `${firstName} ${lastName}`;
+  return email || '';
+}
+
+// export const generatePortfolioOwnership = (data: {userId?: number | null, portfolio?: Portfolio | null}): PortfolioOwnership => {
+//   const { portfolio, userId } = data;
+//   if (!userId || !portfolio) return PortfolioOwnership.Personal;
+
+//   if (portfolio.pmId && portfolio.pmId === userId) return PortfolioOwnership.Managing;
+//   if (portfolio.pmId && portfolio.pmId !== userId) return PortfolioOwnership.Managed;
+//   return PortfolioOwnership.Personal;
+// }
+
+export const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
