bbfbaa577d01589dfd8163e76ce13729b6617098
Jan Novak <novak.jan90@gmail.com>
2023-03-25 16:00:17 +0100

    feat: Create basic structure for CreatePortfolio page

---
+++ src/pages/CreatePortfolio/CreatePortfolio.styles.ts
@@ -0,0 +1,9 @@
+import { styled } from "solid-styled-components";
+
+interface IStyledCreatePortfolioProps {
+
+}
+
+export const StyledCreatePortfolio = styled('div') <IStyledCreatePortfolioProps>`
+
+`;
---
+++ src/pages/CreatePortfolio/CreatePortfolio.tsx
@@ -0,0 +1,17 @@
+import { Component } from "solid-js";
+import { StyledCreatePortfolio } from "./CreatePortfolio.styles";
+
+interface ICreatePortfolioProps {
+
+}
+
+const CreatePortfolio: Component<ICreatePortfolioProps> = (props) => {
+
+  return (
+    <StyledCreatePortfolio>
+
+    </StyledCreatePortfolio>
+  )
+}
+
+export default CreatePortfolio;
