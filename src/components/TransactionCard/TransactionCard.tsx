cc76d89358634ca5826112845413018cf9181336
Jan Novak <novak.jan90@gmail.com>
2023-04-23 23:45:59 +0200

    feat: Create TransactionCard component basic structure

---
+++ src/components/TransactionCard/TransactionCard.tsx
@@ -0,0 +1,10 @@
+import { Component } from "solid-js";
+
+
+const TransactionCard: Component = (props) => {
+  return (
+    <div>TransactionCard</div>
+  )
+}
+
+export default TransactionCard;
---
+++ src/components/TransactionCard/index.ts
@@ -0,0 +1,3 @@
+import TransactionCard from "./TransactionCard";
+
+export default TransactionCard;
